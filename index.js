498e3789dc6e106567f6dabd0844a16ce0d80e37
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-12 13:47:23 -0500

    commit tree 508731ce506a681b1021ecf862066aaa6a3039cb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records) {
+  const win = records.find(record => record.result === 'W')
+  if (win) {
+    return win.year;
+  } else {
+    return win;
+  }
+}
